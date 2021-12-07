03d7440f6a695ee5b931bb541b3f84fd0aba53b3
singularityRx0 <94662590+singularityRx0@users.noreply.github.com>
2021-12-07 14:49:24 +0800

    Add files via upload

---
+++ dataType.h
@@ -0,0 +1,45 @@
+#ifndef H_dataType
+#define H_dataType
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class dataType
+{
+	friend ostream& operator<< (ostream&, const dataType&);
+
+public:
+	void setDataInfo(string date, string item, int quantity, int unitprice, int amount);
+
+	void printDate() const;
+
+	void printInfo() const;
+
+	bool checkDate(string date);
+
+	string getDate() const;
+
+	dataType(string date = "", string item = "", int quantity = 0, int unitprice = 0, int amount = 0);
+
+	bool operator==(const dataType&) const;
+	bool operator!=(const dataType&) const;
+
+private:
+	string Date;
+
+	string Item;
+
+	int Quantity;
+
+	int Unitprice;
+
+	int Amount;
+
+
+
+};
+
+
+#endif
