8f57413bf9f482703ac9635128b8ded76d77b32c
singularityRx0 <94662590+singularityRx0@users.noreply.github.com>
2022-01-05 15:43:23 +0800

    Update 06_dataListType.cpp

--- 06_dataListType.cpp
+++ 06_dataListType.cpp
@@ -4,41 +4,6 @@
 
 using namespace std;
 
-void dataListType::searchDataList(string date, bool& found, nodeType<dataType>*& current) const
-{
-	found = false;
-
-	current = first;
-
-	while (current != nullptr && !found)
-	{
-		if (current->info.checkDate(date))
-			found = true;
-		else
-			current = current->link;
-	}
-}
-
-bool dataListType::dataCheckDate(string date) const
-{
-	bool found = false;
-	nodeType<dataType> *location;
-
-	searchDataList(date, found, location);
-
-	return found;
-}
-
-bool dataListType::dateSearch(string date) const
-{
-	bool found = false;
-	nodeType<dataType> *location;
-
-	searchDataList(date, found, location);
-
-	return found;
-}
-
 void dataListType::dataPrintDate() const
 {
 	nodeType<dataType>* current;
